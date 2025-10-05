15a27079fe615a869cb4bc78a55312a44b89b3a5
Shyamsundaar2010 <shyamsundaar2010@gmail.com>
2025-10-05 13:20:40 +0530

    changes in config.py

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ DB_PASS = "1007"
 DB_HOST = "localhost"
 DB_NAME = "wallet_db"'''
 
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:1007@localhost/wallet_db'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:db_password@localhost/wallet_db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = os.urandom(24)
